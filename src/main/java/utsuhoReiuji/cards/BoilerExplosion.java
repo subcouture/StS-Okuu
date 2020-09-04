23f00a4d754f61c58092c0d18feda6d0418bd1be
Linzi Zhu <linzi.zhu@hotmail.com>
2020-09-05 02:00:56 +1000

    Added Some Placeholder Images, beginning of the other archetype of cards.

---
+++ src/main/java/utsuhoReiuji/cards/BoilerExplosion.java
@@ -0,0 +1,65 @@
+package utsuhoReiuji.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import utsuhoReiuji.OkuuMod;
+import utsuhoReiuji.characters.TheDefault;
+
+import static utsuhoReiuji.OkuuMod.makeCardPath;
+
+// public class BoilerExplosion extends AbstractDynamicCard
+public class BoilerExplosion extends AbstractDynamicCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = OkuuMod.makeID(BoilerExplosion.class.getSimpleName());
+    public static final String IMG = makeCardPath("BoilerExplosion.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.COMMON; //  Up to you, I like auto-complete on these
+    private static final CardTarget TARGET = CardTarget.ENEMY;  //   since they don't change much.
+    private static final CardType TYPE = CardType.ATTACK;       //
+    public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+
+    private static final int COST = 2;
+    private static final int UPGRADED_COST = 2;
+
+    private static final int DAMAGE = 20;
+    private static final int UPGRADE_PLUS_DMG = 20;
+
+    // /STAT DECLARATION/
+
+
+    public BoilerExplosion() { // public BoilerExplosion() - This one and the one right under the imports are the most important ones, don't forget them
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = DAMAGE;
+    }
+
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(
+                new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
+    }
+
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DMG);
+            upgradeBaseCost(UPGRADED_COST);
+            initializeDescription();
+        }
+    }
+}
