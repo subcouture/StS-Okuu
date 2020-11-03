b9821bacacb9a4a8c5c499516dbc9054db3c597b
Linzi Zhu <linzi.zhu@hotmail.com>
2020-11-04 10:41:18 +1100

    Added Self Tokamak - Worried about my layout for new powers - will probably have to refactor later. Should consider changing the power strings now.

---
+++ src/main/java/utsuhoReiuji/powers/SelfTokamakPower.java
@@ -0,0 +1,68 @@
+package utsuhoReiuji.powers;
+
+import basemod.interfaces.CloneablePowerInterface;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.actions.watcher.PressEndTurnButtonAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import utsuhoReiuji.OkuuMod;
+import utsuhoReiuji.util.TextureLoader;
+
+import static utsuhoReiuji.OkuuMod.makePowerPath;
+
+public class SelfTokamakPower extends AbstractPower implements CloneablePowerInterface {
+    public AbstractCreature source;
+
+    public static final String POWER_ID = OkuuMod.makeID("SelfTokamakPower");
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    private static final Texture tex84 = TextureLoader.getTexture(makePowerPath("SelfTokamakPower_power84.png"));
+    private static final Texture tex32 = TextureLoader.getTexture(makePowerPath("SelfTokamakPower_power32.png"));
+
+    public SelfTokamakPower(final AbstractCreature owner, final AbstractCreature source, final int amount) {
+        name = NAME;
+        ID = POWER_ID;
+
+        this.owner = owner;
+        this.amount = amount;
+        this.source = source;
+
+        type = PowerType.BUFF;
+        isTurnBased = false;
+
+        this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
+        this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
+
+        updateDescription();
+    }
+
+    @Override
+    public void atStartOfTurn() {
+        this.flash();
+        AbstractDungeon.actionManager.addToBottom(new GainBlockAction(owner, owner, amount));
+        AbstractDungeon.actionManager.addToBottom(new RemoveSpecificPowerAction(owner, owner, this));
+    }
+
+    @Override
+    public void updateDescription() {
+        if (amount == 1) {
+            description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1];
+        } else if (amount > 1) {
+            description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[2];
+        }
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new SelfTokamakPower(owner, source, amount);
+    }
+}
