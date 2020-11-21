0219a5b9db7dc27bf83edea9dce856809cb623cc
Linzi Zhu <linzi.zhu@hotmail.com>
2020-11-21 17:12:59 +1100

    Set up GluttonousCrow to add ethereal on use

---
+++ src/main/java/utsuhoReiuji/actions/GluttonousCrowFollowUpAction.java
@@ -0,0 +1,36 @@
+package utsuhoReiuji.actions;
+
+import basemod.abstracts.AbstractCardModifier;
+import basemod.helpers.CardModifierManager;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.GameActionManager;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.utility.WaitAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import utsuhoReiuji.cardMods.EtherealCardMod;
+
+import java.util.Iterator;
+
+public class GluttonousCrowFollowUpAction extends AbstractGameAction {
+    public GluttonousCrowFollowUpAction() {
+        this.duration = 0.001F;
+    }
+
+    public void update() {
+        AbstractDungeon.actionManager.addToTop(new WaitAction(0.4F));
+        this.tickDuration();
+        if (this.isDone) {
+            Iterator<AbstractCard> var1 = DrawCardAction.drawnCards.iterator();
+
+            EtherealCardMod mod = new EtherealCardMod();
+
+            while (var1.hasNext()) {
+                AbstractCard card = var1.next();
+                if (mod.isApplicable(card)) {
+                    CardModifierManager.addModifier(card, mod);
+                }
+            }
+        }
+    }
+}
