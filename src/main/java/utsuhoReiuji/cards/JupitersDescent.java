acdeba4c7a8bd3fa13886d36a91b10493d9dc126
subcouture <linzi.zhu@hotmail.com>
2021-02-18 03:00:44 +1100

    Added the base file for Jupiters Descent, creating the power tomorrow.

---
+++ src/main/java/utsuhoReiuji/cards/JupitersDescent.java
@@ -0,0 +1,57 @@
+package utsuhoReiuji.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import utsuhoReiuji.OkuuMod;
+import utsuhoReiuji.cards.abstractCards.AbstractDynamicCard;
+import utsuhoReiuji.characters.UtsuhoReiuji;
+
+import static utsuhoReiuji.OkuuMod.makeCardPath;
+
+// public class JupitersDescent extends AbstractDynamicCard
+public class JupitersDescent extends AbstractDynamicCard {
+
+    public static final String ID = OkuuMod.makeID(JupitersDescent.class.getSimpleName());
+    public static final String IMG = makeCardPath("JupitersDescent.png");// "public static final String IMG = makeCardPath("JupitersDescent.png");
+
+    private static final CardRarity RARITY = CardRarity.COMMON; //  Up to you, I like auto-complete on these
+    private static final CardTarget TARGET = CardTarget.ENEMY;  //   since they don't change much.
+    private static final CardType TYPE = CardType.ATTACK;       //
+    public static final CardColor COLOR = UtsuhoReiuji.Enums.REIUJI_GREEN;
+
+    private static final int COST = 2;
+    private static final int UPGRADED_COST = 1;
+
+    private static final int DAMAGE = 2;
+    private static final int UPGRADE_PLUS_DMG = 0;
+
+
+    public JupitersDescent() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = DAMAGE;
+    }
+
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(
+                new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
+    }
+
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DMG);
+            upgradeBaseCost(UPGRADED_COST);
+            initializeDescription();
+        }
+    }
+}
