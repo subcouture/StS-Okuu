fdfaff623b50df2125d194549b46fd412bc20e34
Linzi Zhu <linzi.zhu@hotmail.com>
2021-05-10 17:00:17 +1000

    Added 25 additional card art possibilties, copied the code for Scry over as the basis for Jupiter's Descent

--- src/main/java/utsuhoReiuji/actions/JupitersDescentAction.java
+++ src/main/java/utsuhoReiuji/actions/JupitersDescentAction.java
@@ -4,13 +4,19 @@ import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.UIStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
 
 import java.util.Iterator;
 
 public class JupitersDescentAction extends AbstractGameAction {
     private float startingDuration;
+    private static final UIStrings uiStrings = CardCrawlGame.languagePack
+            .getUIString("ExhaustAction");
+    public static final String[] UISTRING = uiStrings.TEXT;
 
     public JupitersDescentAction(AbstractCreature target){
         this.actionType = ActionType.WAIT;
@@ -18,18 +24,26 @@ public class JupitersDescentAction extends AbstractGameAction {
         this.source = AbstractDungeon.player;
         this.target = target;
         this.duration = this.startingDuration;
+        this.amount = 2;
     }
 
 
+
+    //Copied from ScryAction, needs changing to match intended behaviour
     @Override
     public void update() {
-        if (AbstractDungeon.getMonsters().areMonstersBasicallyDead()){
+        if (AbstractDungeon.getMonsters().areMonstersBasicallyDead()) {
             this.isDone = true;
         } else {
             Iterator var1;
-            AbstractCard card;
-            if(this.duration == this.startingDuration) {
+            AbstractCard c;
+            if (this.duration == this.startingDuration) {
                 var1 = AbstractDungeon.player.powers.iterator();
+
+                while(var1.hasNext()) {
+                    AbstractPower p = (AbstractPower)var1.next();
+                    p.onScry();
+                }
 
                 if (AbstractDungeon.player.drawPile.isEmpty()) {
                     this.isDone = true;
@@ -37,8 +51,39 @@ public class JupitersDescentAction extends AbstractGameAction {
                 }
 
                 CardGroup tmpGroup = new CardGroup(CardGroup.CardGroupType.UNSPECIFIED);
+                if (this.amount != -1) {
+                    for(int i = 0; i < Math.min(this.amount, AbstractDungeon.player.drawPile.size()); ++i) {
+                        tmpGroup.addToTop((AbstractCard)AbstractDungeon.player.drawPile.group.get(AbstractDungeon.player.drawPile.size() - i - 1));
+                    }
+                } else {
+                    Iterator<AbstractCard> var5 = AbstractDungeon.player.drawPile.group.iterator();
 
+                    while(var5.hasNext()) {
+                        c = (AbstractCard)var5.next();
+                        tmpGroup.addToBottom(c);
+                    }
+                }
+
+                AbstractDungeon.gridSelectScreen.open(tmpGroup, this.amount, true, UISTRING[0]);
+            } else if (!AbstractDungeon.gridSelectScreen.selectedCards.isEmpty()) {
+                var1 = AbstractDungeon.gridSelectScreen.selectedCards.iterator();
+
+                while(var1.hasNext()) {
+                    c = (AbstractCard)var1.next();
+                    AbstractDungeon.player.drawPile.moveToDiscardPile(c);
+                }
+
+                AbstractDungeon.gridSelectScreen.selectedCards.clear();
             }
+
+            var1 = AbstractDungeon.player.discardPile.group.iterator();
+
+            while(var1.hasNext()) {
+                c = (AbstractCard)var1.next();
+                c.triggerOnScry();
+            }
+
+            this.tickDuration();
         }
     }
 }
