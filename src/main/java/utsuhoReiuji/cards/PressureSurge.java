03d72c7ca2eb84ea6e14a9a7bfc86cce3dfdaf50
subcouture <linzi@particularaudience.com>
2022-08-24 14:35:11 +1000

    Added Pressure Surge, a card that turns heat in to damage.

---
+++ src/main/java/utsuhoReiuji/cards/PressureSurge.java
@@ -0,0 +1,56 @@
+package utsuhoReiuji.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import utsuhoReiuji.OkuuMod;
+import utsuhoReiuji.cards.abstractCards.AbstractDynamicCard;
+import utsuhoReiuji.characters.UtsuhoReiuji;
+
+import static utsuhoReiuji.OkuuMod.makeCardPath;
+
+// public class PressureSurge extends AbstractDynamicCard
+public class PressureSurge extends AbstractDynamicCard {
+
+    // Deal damage equal to Heat, then lose all Heat.
+
+    public static final String ID = OkuuMod.makeID(PressureSurge.class.getSimpleName());
+    public static final String IMG = makeCardPath("PressureSurge.png");
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = UtsuhoReiuji.Enums.REIUJI_GREEN;
+
+    private static final int COST = 2;
+    private static final int UPGRADED_COST = 1;
+
+
+    public PressureSurge() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+    }
+
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        if(p.hasPower("HeatPower")) {
+            AbstractDungeon.actionManager.addToBottom(
+                    new DamageAction(m, new DamageInfo(p, p.getPower("HeatPower").amount, damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_HEAVY));
+        }
+    }
+
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBaseCost(UPGRADED_COST);
+            initializeDescription();
+        }
+    }
+}
