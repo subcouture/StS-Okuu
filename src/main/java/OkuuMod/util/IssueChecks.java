240d082f831099469ea14bb28f77c6855548a6fc
subcouture <linzi@particularaudience.com>
2025-08-20 20:01:09 +1000

    Rebase from DefaultMod to new BasicMod framework, just in case there's been changes in the past 3 years.

---
+++ src/main/java/OkuuMod/util/IssueChecks.java
@@ -0,0 +1,28 @@
+package OkuuMod.util;
+
+import OkuuMod.OkuuMod;
+import com.badlogic.gdx.Files;
+import com.badlogic.gdx.backends.lwjgl.LwjglFileHandle;
+import com.badlogic.gdx.files.FileHandle;
+import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+
+/**
+ * A class containing utility methods for checking and logging common issues.
+ */
+@SpireInitializer
+public class IssueChecks {
+    public static void initialize() {
+        characterAssetsCheck();
+    }
+
+
+
+    private static void characterAssetsCheck() {
+        boolean validCharAssets = false;
+
+        FileHandle charResources = new LwjglFileHandle(OkuuMod.characterPath(""), Files.FileType.Internal);
+        if (charResources.exists() && charResources.isDirectory()) {
+            validCharAssets = true;
+        }
+    }
+}
---
+++ src/main/java/OkuuMod/util/Sounds.java
@@ -0,0 +1,8 @@
+package OkuuMod.util;
+
+import static OkuuMod.OkuuMod.audioPath;
+
+public class Sounds {
+    public static String TEST_SOUND = audioPath("test.wav"); //Load audio using a given path
+    public static String ding; //Load audio from audioPath based on the field name
+}
